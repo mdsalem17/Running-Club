9f13d74b4c29cf94dd0d20317350ecffc3b259ab
MESSOUD <mohamed-salem.messoud@etu.univ-lyon1.fr>
2019-04-25 22:05:25 +0200

    Courses de l'adherent

---
+++ projetlifbdw/adh/courses.php
@@ -0,0 +1,33 @@
+<?php $status = session_status();
+if($status == PHP_SESSION_NONE){
+    //There is no active session
+    session_start();}
+if (file_exists('../includes/functions.php')) {
+    include_once('../includes/functions.php');}
+
+    function get_liste_des_courses_adherents(){
+        $query = "SELECT Ed.anneeEdition, Ep.type, C.nomC FROM Epreuve Ep, Edition Ed, Course C
+        WHERE Ep.idEpreuve IN (SELECT idEpreuve FROM Participation
+                            WHERE prenom='Celine' AND nom='Roula')
+        AND Ed.idEdition=Ep.idEdition AND Ep.idCourse=C.idCourse
+    ORDER BY Ed.anneeEdition DESC; ";
+        $res=traiterRequeteK($query);
+        
+        return Array2Table($res);
+    }
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <meta charset="utf-8">
+        
+        <title>projetLIFBDW1</title>
+        <script src ="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.js"></script>
+        
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script type="text/javascript">
+</html>
